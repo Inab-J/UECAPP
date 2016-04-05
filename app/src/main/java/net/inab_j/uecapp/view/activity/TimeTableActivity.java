dd3deb806e7b6302cd2bca9ad4be4f0fc44cf8a8
Inaba <n2.jima@gmail.com>
2016-04-06 01:56:44 +0900

    大学院対応 時間割をスクロール可能に

--- app/src/main/java/net/inab_j/uecapp/view/activity/TimeTableActivity.java
+++ app/src/main/java/net/inab_j/uecapp/view/activity/TimeTableActivity.java
@@ -1,9 +1,11 @@
 package net.inab_j.uecapp.view.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Environment;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.Snackbar;
@@ -20,6 +22,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 
@@ -28,11 +31,14 @@ import net.inab_j.uecapp.R;
 import net.inab_j.uecapp.model.System.IdCorrespond;
 
 import java.io.File;
+import java.util.Map;
 
 public class TimeTableActivity extends AppCompatActivity {
 
     private final String SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/UECApp/";
     private CoordinatorLayout mCoordinatorLayout;
+    private int mBelong;
+    private int mMajor;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -58,6 +64,10 @@ public class TimeTableActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        mBelong = Integer.parseInt(pref.getString("pref_user_belong", "0"));
+        mMajor = Integer.parseInt(pref.getString("pref_user_major", "-1"));
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -76,7 +86,10 @@ public class TimeTableActivity extends AppCompatActivity {
     public void onBtnClick(View v) {
         Log.d("dbg", IdCorrespond.ID_TO_NAME.get(v.getId()));
         File file = new File(SAVE_DIR + IdCorrespond.ID_TO_NAME.get(v.getId()));
-
+        /*
+        TODO: IdCorrespondを大学院の前後期に対応させる
+        TODO: 対応させたら表示しているページに対応したものでファイル名に変換する
+        */
         if (file.exists()) {
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(Uri.parse("file://" + file.getPath()), "application/pdf");
@@ -87,6 +100,31 @@ public class TimeTableActivity extends AppCompatActivity {
         }
     }
 
+    public void onGraduateBtnClick(View v) {
+        Log.d("dbg", IdCorrespond.ID_TO_NAME_GRADUATE.get(v.getId()));
+        String filename = IdCorrespond.ID_TO_NAME_GRADUATE.get(v.getId());
+        if (!filename.endsWith("pdf")) {
+            switch (mViewPager.getCurrentItem()) {
+                case 0:
+                    filename += "zen.pdf";
+                    break;
+                case 1:
+                    filename += "kou.pdf";
+                    break;
+            }
+        }
+
+        Log.d("dbg", filename);
+        File file = new File(SAVE_DIR + filename);
+        if (file.exists()) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(Uri.parse("file://" + file.getPath()), "application/pdf");
+            startActivity(intent);
+        } else {
+            new GetPDFTask(this, filename).execute();
+            CreateSnackbar(getString(R.string.timet_getting));
+        }
+    }
 
     /**
      * A placeholder fragment containing a simple view.
@@ -97,6 +135,8 @@ public class TimeTableActivity extends AppCompatActivity {
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final String ARG_BELONG = "user_belong";
+        private static final String ARG_MAJOR = "user_major";
 
         public PlaceholderFragment() {
         }
@@ -105,10 +145,12 @@ public class TimeTableActivity extends AppCompatActivity {
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int sectionNumber, int belong, int major) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putInt(ARG_BELONG, belong);
+            args.putInt(ARG_MAJOR, major);
             fragment.setArguments(args);
             return fragment;
         }
@@ -117,12 +159,30 @@ public class TimeTableActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView;
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
-                rootView = inflater.inflate(R.layout.fragment_day_first, container, false);
-            } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
-                rootView = inflater.inflate(R.layout.fragment_day_second, container, false);
+            if (getArguments().getInt(ARG_BELONG) < 3) {
+                if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+                    rootView = inflater.inflate(R.layout.fragment_day_first, container, false);
+                } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
+                    rootView = inflater.inflate(R.layout.fragment_day_second, container, false);
+                } else {
+                    rootView = inflater.inflate(R.layout.fragment_night, container, false);
+                }
             } else {
-                rootView = inflater.inflate(R.layout.fragment_night, container, false);
+                if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
+                    rootView = inflater.inflate(R.layout.fragment_ie, container, false);
+                } else {
+                    rootView = inflater.inflate(R.layout.fragment_ie, container, false);
+                    ((TextView) rootView.findViewById(R.id.is)).setText(
+                            getResources().getText(R.string.timet_is_2nd));
+                    rootView.findViewById(R.id.master_1st_eng_after).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.master_1st_eng_before).setVisibility(View.GONE);
+                }
+
+                // IEの専攻が登録されていれば専攻別ボタンを表示
+                int major = getArguments().getInt(ARG_MAJOR);
+                for (int id: IdCorrespond.MAJOR_TO_BUTTON_ID.get(major)) {
+                    rootView.findViewById(id).setVisibility(View.VISIBLE);
+                }
             }
             return rootView;
         }
@@ -142,24 +202,36 @@ public class TimeTableActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return PlaceholderFragment.newInstance(position + 1, mBelong, mMajor);
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            if (mBelong < 3) {
+                return 3;
+            } else {
+                return 2;
+            }
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "昼間前期";
-                case 1:
-                    return "昼間後期";
-                case 2:
-                    return "夜間";
+            if (mBelong < 3) {
+                switch (position) {
+                    case 0:
+                        return "昼間前期";
+                    case 1:
+                        return "昼間後期";
+                    case 2:
+                        return "夜間";
+                }
+            } else {
+                switch (position) {
+                    case 0:
+                        return "前期";
+                    case 1:
+                        return "後期";
+                }
             }
             return null;
         }
