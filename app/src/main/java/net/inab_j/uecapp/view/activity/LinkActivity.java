616a19d4853ccf802c0ffe485df11e900e838a7c
Inaba <n2.jima@gmail.com>
2016-04-06 03:30:30 +0900

    リンクを編集

--- app/src/main/java/net/inab_j/uecapp/view/activity/LinkActivity.java
+++ app/src/main/java/net/inab_j/uecapp/view/activity/LinkActivity.java
@@ -14,10 +14,6 @@ import net.inab_j.uecapp.R;
 
 public class LinkActivity extends AppCompatActivity {
 
-    private final String AT_UNIV = "http://uec.atuniv.jp/";
-    private final String CAMPUS_WEB = "https://campusweb.office.uec.ac.jp/campusweb/";
-    private final String WEBCLASS = "https://webclass.cdel.uec.ac.jp/";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +24,37 @@ public class LinkActivity extends AppCompatActivity {
     }
 
     public void onLinkClick(View v) {
+        final String UEC = "http://www.uec.ac.jp";
+        final String CAMPUS_WEB = "https://campusweb.office.uec.ac.jp/campusweb/";
+        final String AT_UNIV = "http://uec.atuniv.jp/";
+        final String WEBCLASS = "https://webclass.cdel.uec.ac.jp/";
+        final String COOP = "http://www.univcoop.jp/uec/";
+        final String INFORMATION = "https://www.cc.uec.ac.jp";
+
         Intent i = null;
         switch (v.getId()) {
+            case R.id.uec:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(UEC));
+                break;
+
             case R.id.campus_web:
                 i = new Intent(Intent.ACTION_VIEW, Uri.parse(CAMPUS_WEB));
                 break;
+
             case R.id.at_univ:
                 i = new Intent(Intent.ACTION_VIEW, Uri.parse(AT_UNIV));
                 break;
+
             case R.id.webclass:
                 i = new Intent(Intent.ACTION_VIEW, Uri.parse(WEBCLASS));
+                break;
+
+            case R.id.coop:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(COOP));
+                break;
+
+            case R.id.information:
+                i = new Intent(Intent.ACTION_VIEW, Uri.parse(INFORMATION));
                 break;
         }
 
